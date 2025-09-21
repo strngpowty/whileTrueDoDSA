8faca818bd03371627555f5d04b9ed6fa3906984
strngpowty <strngpowty@gmail.com>
2025-09-21 18:23:55 +0530

    WIP: DSA patterns

--- patterns/square-hollow-pattern.js
+++ patterns/square-hollow-pattern.js
@@ -8,3 +8,16 @@
 * * * * * *
 
 */
+
+let n = 6
+
+for ( let i=0; i<n; i++) {
+    for (let j= 0; j< n; j++) {
+        if(i == 0 || i == n-1 || j == 0 || j == n-1) {
+            process.stdout.write("*")
+        } else {
+            process.stdout.write(" ")
+        }
+    }
+    process.stdout.write("\n")
+}
