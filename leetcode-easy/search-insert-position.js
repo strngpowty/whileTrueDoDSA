75bde6abe10aa0665aad7193be3f8f681cd4bce7
strngpowty <strngpowty@gmail.com>
2025-10-04 21:22:17 +0530

    search-insert-position.js

---
+++ leetcode-easy/search-insert-position.js
@@ -0,0 +1,18 @@
+nums = [1,3,5,6], target = 7
+
+let i = 0, j = nums.length-1
+
+
+while(j >= i) {
+    let mid = Math.floor((j+i)/2)
+    if(nums[mid] === target) {
+        console.log(mid)
+        return
+    } else if(nums[mid] < target) {
+        i++
+    } else {
+        j--
+    }
+}
+
+console.log(i)
